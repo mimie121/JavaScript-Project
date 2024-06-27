88051ffb9c48928a599c79aac36c1917e65d485e
mimie121 <talk2mimie.reply@gmail.com>
2024-06-27 13:57:36 +0100

    Roll

---
+++ Random Number Generator Js/main.js
@@ -0,0 +1,21 @@
+const Button = document.getElementById("roll");
+const Labell = document.getElementById("Label1");
+const Label2 = document.getElementById("Label2");
+const Label3 = document.getElementById("Label3");
+
+const min = 1;
+const max = 8;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+Button.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    // randomNum1 = ma
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    Labell.textContent = randomNum1;
+    Label2.textContent = randomNum2;
+    Label3.textContent = randomNum3;
+}
+
